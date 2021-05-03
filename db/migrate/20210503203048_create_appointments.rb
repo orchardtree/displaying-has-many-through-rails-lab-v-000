e763c6f229f3a58f84844e27dd92c1212c230a61
Jesse Wenger <wengerj@gmail.com>
2021-05-03 23:36:22 +0000

    Automatically backed up by Learn

--- db/migrate/20210503203048_create_appointments.rb
+++ db/migrate/20210503203048_create_appointments.rb
@@ -2,8 +2,8 @@ class CreateAppointments < ActiveRecord::Migration[5.0]
   def change
     create_table :appointments do |t|
       t.datetime, :apointment_datetime
-      t.string :doctor_id,
-      t.string :patient_id
+      t.integer :doctor_id,
+      t.integer :patient_id
 
       t.timestamps
     end
